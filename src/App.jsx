40c0c9d9cec11c826cfa2fa4bf414e547729bd76
SantiagoFigli <107778558+SantiagoFigli@users.noreply.github.com>
2024-01-25 12:17:52 -0300

    Mas cambios

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import { TfiGift } from "react-icons/tfi";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
